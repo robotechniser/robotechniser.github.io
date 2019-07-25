b58b0c3b77527d80fe1d4d1104159db7fbaecbac
JeS24 <jyotirmaya.shivottam@niser.ac.in>
2019-07-25 22:16:25 +0530

    Added a "Back to top" button

--- rtc_website/scripts/scripts.js
+++ rtc_website/scripts/scripts.js
@@ -19,3 +19,12 @@ $(document).on('click', 'a[href^="#"]', function(e) {
     // animated top scrolling
     $('body, html').animate({scrollTop: pos});
 });
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("backToTop").style.display = "block";
+  } else {
+    document.getElementById("backToTop").style.display = "none";
+  }
+}
